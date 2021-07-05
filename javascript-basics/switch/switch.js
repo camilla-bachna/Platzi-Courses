bf76fa8fadfc1928b6b77059601fe891d1251916
camilla-bachna <camilla.bachna@gmail.com>
2021-07-05 19:54:52 +0200

    add a switch example

---
+++ javascript-basics/switch/switch.js
@@ -0,0 +1,15 @@
+var numero = 101;
+
+switch (numero) {
+  case 1:
+    console.log("I am a 1");
+    break;
+  case 10:
+    console.log("I am a 10");
+    break;
+  case 100:
+    console.log("I am a 100");
+    break;
+  default:
+    console.log("None of the numbers are correct");
+}
