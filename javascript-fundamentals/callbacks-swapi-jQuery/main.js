95a26c3577bdb84dd34f5c517e1ac5a6c421857d
camilla-bachna <camilla.bachna@gmail.com>
2021-07-22 10:56:43 +0200

    add all characters and their films

--- javascript-fundamentals/callbacks-swapi-jQuery/main.js
+++ javascript-fundamentals/callbacks-swapi-jQuery/main.js
@@ -1,15 +1,26 @@
 const API_URL = "https://swapi.dev/api/";
 const PEOPLE_URL = "people/:id/";
-
-const lukeUrl = `${API_URL}${PEOPLE_URL.replace(":id", 1)}`;
 const options = { crossDomain: true }; //crossDomain => to external page
+
 const onPeopleResponse = function (person) {
-  console.log(`Hola yo soy ${person.name}`);
+  console.log(`Hola, yo soy ${person.name}`);
+  const films = person.films;
+  filmSigularOrPlural(films);
+  console.log(`He actuado ${filmSigularOrPlural(films)}:`);
+  films.map((film) => console.log(film));
 };
 
-$.get(
-  lukeUrl,
-  options,
-  onPeopleResponse //this function will only get executed if the request was successful
-  /* console.log(arguments); //Arguments(3) [{…}, "success", {…}, callee: ƒ, Symbol(Symbol.iterator): ƒ] */
-);
+function filmSigularOrPlural(films) {
+  return films.length === 1
+    ? "el siguiente película"
+    : "en las siguientes películas";
+}
+
+function getACharacter(id) {
+  const url = `${API_URL}${PEOPLE_URL.replace(":id", id)}`;
+  $.get(url, options, onPeopleResponse);
+}
+
+for (let i = 1; i <= 10; i++) {
+  getACharacter(i);
+}
