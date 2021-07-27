97b46df67eb365a6b26e3305f762616fb9780f9a
camilla-bachna <camilla.bachna@gmail.com>
2021-07-27 15:58:38 +0200

    remove a comment

--- javascript-fundamentals/swapi-jQuery-browserify/js/getFilmInfo.js
+++ javascript-fundamentals/swapi-jQuery-browserify/js/getFilmInfo.js
@@ -6,17 +6,14 @@ import getFilmTitle from "./getFilmTitle";
  * build with the array of films an array of Promises (promisesFilms) and pass it as a parameter to Promise.all().
  * @param name: name of Star Wars characters
  * @param films: array of films in which Star Wars character appeared
- * await to control order in which filmtitles are printed
  */
 function getFilmInfo(name, films) {
   console.log(`Enter function getFilmInfo`);
   const promisesFilms = films.map((filmUrl) => getAFilm(filmUrl));
   try {
-    const filmsOfCharacters = Promise.all(promisesFilms).then(
-      (filmsOfCharacters) => {
-        getFilmTitle(filmsOfCharacters, name);
-      }
-    );
+    Promise.all(promisesFilms).then((filmsOfCharacters) => {
+      getFilmTitle(filmsOfCharacters, name);
+    });
   } catch {
     console.log("Error films");
   }
