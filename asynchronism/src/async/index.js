c122a47199029868c85ebca7c1327cb9ed7ae2b2
camilla-bachna <camilla.bachna@gmail.com>
2021-07-30 14:47:51 +0200

    add example async-await and order of  macrotask and microtask

---
+++ asynchronism/src/async/index.js
@@ -0,0 +1,44 @@
+"use strict";
+
+//
+const doSomethingAsync = () => {
+  return new Promise((resolve, reject) => {
+    true
+      ? setTimeout(() => resolve("Do Something Async"), 3000)
+      : reject(new Error("Test Error"));
+  });
+};
+
+//wait till promise is resolved, until the promise is fulfilled
+const doSomething = async () => {
+  const something = await doSomethingAsync();
+  console.log(something);
+};
+
+console.log("Before 1");
+doSomething();
+console.log("After 1");
+
+/* Before 1
+After 1
+Do Something Async (prints this minimum 3 seconds later) */
+
+const anotherFunction = async () => {
+  try {
+    //here could be added how many times I need to call the promise, in this case I only need to call it one time
+    const something = await doSomethingAsync();
+    console.log(something);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+console.log("Before 2");
+anotherFunction();
+console.log("After 2");
+
+/* After 1
+Before 2
+After 2
+Do Something Async (after 3 seconds both appear together)
+Do Something Async */
---
+++ asynchronism/src/promise/microtask-queue.js
@@ -0,0 +1,43 @@
+/* How event loop works */
+
+/* both browser and node js are always running a single threaded event loop to run your code.
+On the fist go around it will run all you synchronous code, 
+but it might also queue up asynchronous events to be called back later */
+
+// L1
+console.log(`Synchronous 1`);
+
+// L1
+//if it is a macrotask like a setTimeout or setInterval it will be executed on the next event loop
+// 0 time delay
+setTimeout((_) => console.log(`Timeout 2`), 0);
+
+// L1
+//but if it is a microtask like a fulfilled promise then it will be called back before the start of the next event loop
+//resolves right away
+Promise.resolve().then((_) => console.log(`Promise 3`));
+
+// L1
+console.log(`Synchronous 4`);
+
+/* This is the order it will appear: nothing has an actual time delay, 
+however each line of code will NOT be executed one by one 
+
+L1: (gets logged up right away, because it is running on the main thread)
+
+L2: (its being queued for a future task - macrotask)
+
+L3: (the promise is queued to run in the microtask queue immediately after this current task. 
+    So even if the setTimeout was queued up before the promise, the promise still gets executed first, 
+    because of the priority of the microtask queue)
+
+L4: (gets executed right away, its running on the main thread)
+
+=>
+
+Synchronous 1 
+Synchronous 4 
+Promise 3
+Timeout 2 
+
+*/
