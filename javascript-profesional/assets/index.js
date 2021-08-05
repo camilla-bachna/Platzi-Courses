a636f43a704b0a224849319cefe1cad7415f6705
camilla-bachna <camilla.bachna@gmail.com>
2021-08-05 16:15:14 +0200

    add explanation about this

--- javascript-profesional/assets/index.js
+++ javascript-profesional/assets/index.js
@@ -12,7 +12,12 @@ const buttonVolumeDown = document.querySelector(".volume-down");
 //create instance of prototype MediaPlayer
 /* one object instead of multiple arguments so we can better understand how our code works, 
 e.g. new MediaPlayer({ element: video, title: "My favorite video", autoplay: true }); */
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] }); //AutoPlay is a plugin, a plugin is a class, instantiate it
+const player = new MediaPlayer({
+  el: video,
+  plugins: [
+    //new AutoPlay()
+  ],
+}); //AutoPlay is a plugin, a plugin is a class, instantiate it
 
 buttonPlay.onclick = () => player.togglePlay(); //start / pause video when clicking on button
 video.onclick = () => player.togglePlay(); //start / pause video when clicking on video
