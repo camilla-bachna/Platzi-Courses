f49253d626498c535faa742c0e47db5aa2cdbb1e
camilla-bachna <camilla.bachna@gmail.com>
2021-09-01 13:56:42 +0200

    add example of stateful and stateless components

---
+++ react-practical/create-react-app/src/App.js
@@ -0,0 +1,14 @@
+import Stateful from "./components/Stateful";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Stateful />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-practical/create-react-app/src/components/Stateful.js
@@ -0,0 +1,38 @@
+//implement the use state method
+import React, { Component, useState } from "react";
+
+/* a React component declaration */
+class Stateful extends Component {
+  /* add logic */
+  constructor(props) {
+    /* initialize them */
+    super(props);
+    /* assign state, components properties */
+    this.state = { hello: "Hello World" };
+  }
+  /* render properties from the state */
+  render() {
+    return <h1>{this.state.hello}</h1>;
+  }
+}
+
+export default Stateful;
+
+/* Though props don't have a local state they can inherit state and life-cycle features using Hooks 
+
+//a React stateless functional component
+const ExampleComponent = (props) => {
+  const alertLocation = (event) => {
+    alert(`This content is from ${location}`);
+  }
+  // when the component is instantiated send the div to the html document
+  return(
+    <div><a href="#" onClick={alertLocation}>Location?</a></div>;
+  )
+}
+
+// functional component declaration
+const ExampleComponent = () => {
+  //set a variable to the useState function invocation
+  const [location, setLocation] = useState("over there");
+};*/
