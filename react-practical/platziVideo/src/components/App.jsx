f205acfe29741017f7d9aa46a4e82f64d51b279b
camilla-bachna <camilla.bachna@gmail.com>
2021-09-02 10:43:36 +0200

    add webpack dev server, estilos con sass

---
+++ react-practical/platziVideo/src/components/App.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "../assets/styles/App.scss";
+
+const App = () => <h1>Hello World</h1>;
+
+export default App;
---
+++ react-practical/platziVideo/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    //where we will save our final files when we compile
+    //resolve allows us to detect the directory we are in and we pass it a directory in which we will save the files
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        //main rule: the identification of our of our files
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  //we pass it an object with the configuration we need
+  plugins: [
+    new HtmlWebpackPlugin({
+      //where the template is located and the filename its going to have
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "assets/[name].css",
+    }),
+  ],
+};
