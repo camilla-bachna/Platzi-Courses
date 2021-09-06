f239dd3b21fa8ccc7fe7139a517f2f88d7e8f4cd
camilla-bachna <camilla.bachna@gmail.com>
2021-09-06 17:29:51 +0200

    install redux

--- react-router-redux/platziVideo/src/index.js
+++ react-router-redux/platziVideo/src/index.js
@@ -1,5 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import App from "./routes/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+/* render receives 2 parameters: The first is our application and the second is where the elements would be rendered.
+The first is our application and the second is where the elements would be rendered. */
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
