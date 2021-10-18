d91d718b3da10c537029d4f1c96e6845be526261
“camillaBa” <camilla.bachna@gmail.com>
2021-10-18 18:22:52 +0200

    use method get and capture parameters coming from URL with req.params

--- API-REST-with-Express.js/my-store/index.js
+++ API-REST-with-Express.js/my-store/index.js
@@ -17,9 +17,31 @@ app.get('/new-route', (req, res) => {
 
 //response in JSON format
 app.get('/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000,
+    },
+    {
+      name: 'Product 2',
+      price: 2000,
+    },
+    {
+      name: 'Product 3',
+      price: 3000,
+    },
+  ]);
+});
+
+app.get('/products/:id', (req, res) => {
+  //I will pick up the id you are sending me and add it to the answer
+  //id because thats how we added it /:id
+  const id = req.params.id;
+  //ES6 const {id} = req.params;
   res.json({
-    name: 'Product 1',
-    price: 1000,
+    id,
+    name: 'Product 2',
+    price: 2000,
   });
 });
 
@@ -47,6 +69,73 @@ app.get('/categories', (req, res) => {
     },
   ]);
 });
+
+app.get('/categories/:categoryId/article/:articleId', (req, res) => {
+  const { categoryId } = req.params;
+  const { articleId } = req.params;
+  res.json({
+    categoryId,
+    category: 'Living room',
+    subcategory: 'Furniture',
+    articleId,
+    article: 'Table',
+  });
+});
+
+/* http://localhost:3000/categories/12/article/22
+{
+  "categoryId": "12",
+  "category": "Living room",
+  "subcategory": "Furniture",
+  "articleId": "22",
+  "article": "Table"
+} */
+
+app.get('/users', (req, res) => {
+  res.json([
+    {
+      userName: 'Sofia',
+      email: 'sofia.ejemplo@gmail.com',
+      password: '123456',
+    },
+    {
+      userName: 'Fernando',
+      email: 'fernamdo.torres@gmail.com',
+      password: 'Supersafe',
+    },
+  ]);
+});
+
+app.get(
+  '/users/:userId/orders/:orderId/categories/:categoryId/articles/:articleId',
+  (req, res) => {
+    const { userId, orderId, categoryId, articleId } = req.params;
+    res.json({
+      user: 'Isabela',
+      userId,
+      order: 'Order 1',
+      orderId,
+      categoryId,
+      category: 'Living room',
+      subcategory: 'Furniture',
+      articleId,
+      article: 'Table',
+    });
+  }
+);
+
+/* http://localhost:3000/users/1/orders/2/categories/14/articles/32
+{
+  "user": "Isabela",
+  "userId": "1",
+  "order": "Order 1",
+  "orderId": "2",
+  "categoryId": "14",
+  "category": "Living room",
+  "subcategory": "Furniture",
+  "articleId": "32",
+  "article": "Table"
+} */
 
 //we tell the app which port to listen on
 //we also create a callback that will let us know when it is running
